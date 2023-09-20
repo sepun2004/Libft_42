5e0640c20d13368f584f21607efafdb41f240194
Sebastian Pun <sepun@c2r14s6.42madrid.com>
2023-09-20 15:17:11 +0200

    a

---
+++ ft_memcmp.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sepun <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/18 22:16:07 by sepun             #+#    #+#             */
+/*   Updated: 2023/09/19 15:48:02 by sepun            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	*ss1;
+	const unsigned char	*ss2;
+	size_t				i;
+
+	i = 0;
+	ss1 = (const unsigned char *)s1;
+	ss2 = (const unsigned char *)s2;
+	while (i < n)
+	{
+		if (ss1[i] != ss2[i])
+			return (ss1[i] - ss2[i]);
+		i++;
+	}
+	return (0);
+}
---
+++ ft_strlcpy.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sepun <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/14 21:04:42 by sepun             #+#    #+#             */
+/*   Updated: 2023/09/14 21:05:03 by sepun            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	count;
+	unsigned int	i;
+
+	count = 0;
+	while (src[count] != '\0')
+		count++;
+	i = 0;
+	while (src[i] != '\0' && i < (size - 1))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (count);
+}
