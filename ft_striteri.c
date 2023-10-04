aab074a90dd718650296af7e9bc7ff048f8f4aae
root <root@LAPTOP-06PFH3P2>
2023-10-04 13:19:38 +0200

    4 de Octubre

--- ft_striteri.c
+++ ft_striteri.c
@@ -9,8 +9,31 @@
 /*   Updated: 2023/10/03 13:31:33 by sepun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-void ft_striteri(char *s, void (*f)(unsigned int,
-char*))
-{
+#include "libft.h"
 
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
+{
+	unsigned int	i;
+
+	if (!s || !f)
+		return ;
+	i = 0;
+	while (s[i])
+	{
+		f(i, s + i);
+		i++;
+	}
 }
+
+
+/*void print_index_char(unsigned int index, char *c) {
+    printf("Índice: %u, Carácter: %c\n", index, *c);
+}
+
+int main() {
+    char str[] = "Ejemplo";
+
+    ft_striteri(str, print_index_char);
+
+    return 0;
+}*/
