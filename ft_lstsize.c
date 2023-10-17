8e68726551bcc392f084ecb38541e50fd175aa3c
sepun2004 <145572771+sepun2004@users.noreply.github.com>
2023-10-17 21:18:13 +0200

    17 de Octubre

---
+++ ft_lstsize.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sepun <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/20 15:02:33 by sepun             #+#    #+#             */
+/*   Updated: 2023/09/25 14:58:20 by sepun            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+int ft_lstsize(t_list *lst)
+{
+	int		count;
+	t_list	*temp;
+
+	count = 0;
+	temp = lst;
+	while (temp->next != NULL)
+	{
+		count++;
+		temp = temp->next;
+	}
+	if (temp->next == NULL)
+		count++;
+	return (count);
+}
