51c72695c76f0e67b16f621f54817a34b12b3bd1
sepun2004 <145572771+sepun2004@users.noreply.github.com>
2023-10-18 12:14:58 +0200

    18 de octubre

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,12 +6,12 @@
 /*   By: sepun <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 13:29:03 by sepun             #+#    #+#             */
-/*   Updated: 2023/10/03 13:31:33 by sepun            ###   ########.fr       */
+/*   Updated: 2023/10/18 12:14:15 by sepun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 void ft_putchar_fd(char c, int fd)
 {
-    write(fd, "c", 1);
+    write(fd, &c, 1);
 }
